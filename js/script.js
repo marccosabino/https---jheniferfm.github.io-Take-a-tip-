e2bd678b7d0f4528429a161ca2ec0d8b97cc2b1c
marccosabino <marco.sabinosr@gmail.com>
2024-11-18 08:33:03 -0300

    Update 2

--- js/script.js
+++ js/script.js
@@ -9,15 +9,15 @@ function closeModal(modalId) {
 }
 // Função para criar o efeito do carrossel (movimento automático)
 let index = 0;
-const images = document.querySelectorAll('.carrossel-images img');
+const images = document.querySelectorAll('.carousel-images img');
 const totalImages = images.length;
 
 // Função para mover o carrossel automaticamente
-function moveCarrossel() {
+function moveCarousel() {
     index = (index + 1) % totalImages;  // Move para a próxima imagem
     const offset = -index * 100;  // Mover as imagens para a esquerda
-    document.querySelector('.carrossel-images').style.transform = `translateX(${offset}%)`;
+    document.querySelector('.carousel-images').style.transform = `translateX(${offset}%)`;
 }
 
 // Iniciar a rotação automática
-setInterval(moveCarrossel, 3000);  // Trocar de imagem a cada 3 segundos
+setInterval(moveCarousel, 3000);  // Trocar de imagem a cada 3 segundos
